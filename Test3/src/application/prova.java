740065712255c2bbc03bb92ea16bc48a1b220105
LorenzoMM3 <105658120+LorenzoMM3@users.noreply.github.com>
2022-05-16 18:46:09 +0200

    Add files via upload

--- Test3/src/application/prova.java
+++ Test3/src/application/prova.java
@@ -4,10 +4,7 @@ public class prova {
 public static void main(String[] args) {
 	
 	System.out.println("prova github");
-	System.out.println("prova5");
-	System.out.println("lorenzo juventino");
-	System.out.println("Andrea Merda");
-	System.out.println("Ora posso modificare sium");
+	System.out.println("prova4");
 	
 }
 }
