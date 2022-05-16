effefe9c4989340960f8625a9229045b23aa96f8
LorenzoMM3 <105658120+LorenzoMM3@users.noreply.github.com>
2022-05-16 18:45:02 +0200

    Update prova.java

--- Test3/src/application/prova.java
+++ Test3/src/application/prova.java
@@ -7,6 +7,7 @@ public static void main(String[] args) {
 	System.out.println("prova5");
 	System.out.println("lorenzo juventino");
 	System.out.println("Andrea Merda");
+	System.out.println("Ora posso modificare sium");
 	
 }
 }
